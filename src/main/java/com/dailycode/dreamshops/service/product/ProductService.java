babc171e9bd3cbfc678d59600a99176fd53f30f2
Uditi1 <udititiwari@gmail.com>
2025-01-01 09:12:13 +0530

    feat:create product interface implementing menthod process

--- src/main/java/com/dailycode/dreamshops/service/product/ProductService.java
+++ src/main/java/com/dailycode/dreamshops/service/product/ProductService.java
@@ -1,5 +1,75 @@
 package com.dailycode.dreamshops.service.product;
 
-public class ProductService {
+import java.util.List;
+
+import com.dailycode.dreamshops.model.Product;
+
+public class ProductService implements IProductService {
+
+    @Override
+    public Product addProduct(Product product) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'addProduct'");
+    }
+
+    @Override
+    public Product getProductById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getProductById'");
+    }
+
+    @Override
+    public void deleteProductById(Long id) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'deleteProductById'");
+    }
+
+    @Override
+    public void updateProduct(Product product, Long productId) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'updateProduct'");
+    }
+
+    @Override
+    public List<Product> getAllProducts() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getAllProducts'");
+    }
+
+    @Override
+    public List<Product> getProductsByCategory(String category) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getProductsByCategory'");
+    }
+
+    @Override
+    public List<Product> getProductsByBrand(String brand) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getProductsByBrand'");
+    }
+
+    @Override
+    public List<Product> getProductsByCategoryAndBrand(String category, String brand) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getProductsByCategoryAndBrand'");
+    }
+
+    @Override
+    public List<Product> getProductsByName(String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getProductsByName'");
+    }
+
+    @Override
+    public List<Product> getProductsByBrandAndName(String brand, String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getProductsByBrandAndName'");
+    }
+
+    @Override
+    public Long countProductsByBrandAndName(String brand, String name) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'countProductsByBrandAndName'");
+    }
 
 }
