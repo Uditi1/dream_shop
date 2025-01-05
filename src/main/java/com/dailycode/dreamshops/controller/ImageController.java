a27318f93860a654e1a4b33b865575022b24e85a
Uditi1 <udititiwari@gmail.com>
2025-01-05 19:56:27 +0530

    feat:delte image controller done

--- src/main/java/com/dailycode/dreamshops/controller/ImageController.java
+++ src/main/java/com/dailycode/dreamshops/controller/ImageController.java
@@ -9,9 +9,11 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -54,6 +56,7 @@ public class ImageController {
         .body(resource);
     }
 
+    @PutMapping("/image/{imageId}/update")
     public ResponseEntity<ApiResponse> updateImage(@PathVariable Long imageId, @RequestBody MultipartFile file){
        try {
         Image image = imageService.getImageById(imageId);
@@ -65,6 +68,21 @@ public class ImageController {
         return  ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
        }
        return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("Update failed!", INTERNAL_SERVER_ERROR));
+
+    }
+
+    @DeleteMapping("/image/{imageId}/delete")
+    public ResponseEntity<ApiResponse> deleteImage(@PathVariable Long imageId){
+       try {
+        Image image = imageService.getImageById(imageId);
+        if(image != null){
+            imageService.deleteImageById(imageId);
+            return ResponseEntity.ok(new ApiResponse("delete success", null));
+        }
+       } catch (ResourceNotFoundException e) {
+        return  ResponseEntity.status(NOT_FOUND).body(new ApiResponse(e.getMessage(), null));
+       }
+       return ResponseEntity.status(INTERNAL_SERVER_ERROR).body(new ApiResponse("delete failed!", INTERNAL_SERVER_ERROR));
 
     }
 
