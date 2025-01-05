acc2e90a84a0aea4a36baffb70421aec0c79b2e0
Uditi1 <udititiwari@gmail.com>
2025-01-05 10:25:17 +0530

    feat: image methods implemented

---
+++ src/main/java/com/dailycode/dreamshops/repository/ImageRepository.java
@@ -0,0 +1,9 @@
+package com.dailycode.dreamshops.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.dailycode.dreamshops.model.Image;
+
+public interface ImageRepository extends JpaRepository<Image, Long>{
+
+}
